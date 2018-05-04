65953e202fb29707f54a0d3cfd36b76a86b3c890
Nathan Khutorskoy <natank44@gmail.com>
2018-05-04 15:37:06 +0300

    Prototype first commit

---
+++ src/prototype/Question.java
@@ -0,0 +1,21 @@
+package prototype;
+
+public class Question {
+	private int ID;
+	private int AutherID;
+	private String question;
+	private String[] Answers;
+	private int CorrectAnswerIndex;
+	
+	public Question(int id, int autherid, String question, String[] answers) {
+		ID=id;
+		AutherID=autherid;
+		this.question = new String(question);
+		Answers = answers;
+		CorrectAnswerIndex=-1;
+	}
+
+	public boolean updateQuestion(int qid, int correctIndex) {
+		
+	}
+}
