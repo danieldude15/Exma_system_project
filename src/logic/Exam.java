16b203e95e2b989f2c8ad4d86ecc714663ac640f
Niv Mizrahi <Niv Mizrahi@10.11.0.133>
2018-05-22 11:37:00 +0300

    added exam

---
+++ src/logic/Exam.java
@@ -0,0 +1,16 @@
+package logic;
+import java.util.ArrayList;
+public class Exam 
+{
+ private int ID;
+ private int Duration;
+ private Teacher Auther;
+ 
+ 
+ public Exam(int id,int duration,Teacher autherid)
+ {
+ ID=id;
+ Duration=duration;
+ Auther=autherid;
+ }
+}
