da4f7fe94903bbcf7dcd1eab4552ece8a3a424b7
Daniel Tibi <cky800@gmail.com>
2018-05-23 22:37:41 +0300

    connected to Tibis Home SQL Server!

--- src/ocsf/server/ServerGlobals.java
+++ src/ocsf/server/ServerGlobals.java
@@ -9,7 +9,7 @@ public class ServerGlobals {
 	public static final String ServerGuiPath = "ServerGui.fxml";
 	public static AESServer server = null;
 	public static ScreensController mainContainer;
-	public static final String dbHost = "localhost/aes";
+	public static final String dbHost = "84.108.116.235/aes";
 	public static final String dbuser="aesUser";
 	public static final String dbpass="QxU&v&HMm0t&";
 	
