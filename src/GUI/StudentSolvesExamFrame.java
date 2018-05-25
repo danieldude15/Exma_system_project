28502b67fbe59ef45ae3b8864d542f9cb832d8c6
איציק <itzikmizrachi710@gmail.com>
2018-05-25 18:50:10 +0300

    Part of Student Gui

---
+++ src/GUI/StudentSolvesExamFrame.java
@@ -0,0 +1,28 @@
+package GUI;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+
+public class StudentSolvesExamFrame implements Initializable {
+
+	@FXML ListView questions;
+	ArrayList<CheckBox> answers;
+	
+
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	
+	
+}
